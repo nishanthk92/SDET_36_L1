ab3db2da559bdc26762ed3ed2eb44902e0636b29
NK Achar <NK Achar@Admin>
2022-07-19 17:42:36 +0530

    First Commit

---
+++ GITHUB_PROJECTS/src/test/java/org/tyss/github/MyFileTest.java
@@ -0,0 +1,24 @@
+package org.tyss.github;
+
+import org.testng.annotations.Test;
+
+public class MyFileTest 
+{
+	@Test
+	public void github1Test()
+	{
+		System.out.println("this is First Github");
+	}
+	@Test
+	public void github2Test()
+	{
+		System.out.println("this is Second Github");
+	}
+	@Test
+	public void github3Test()
+	{
+		System.out.println("this is Third Github");
+	}
+
+
+}
